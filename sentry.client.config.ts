c8b3b6027d5c87e033bae55d15fc69540c26271b
Giorgos Trichopoulos <george.trichopoulos@gmail.com>
2024-07-22 20:33:03 +0300

    set non-error replay rate at 0.1

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -6,18 +6,19 @@ import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
   dsn: "https://a4e338501accee2adf9e58d71b25367e@o4507502891040768.ingest.de.sentry.io/4507503305949264",
+  environment: "sample-nextjs",
 
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: false,
+  debug: true,
 
   replaysOnErrorSampleRate: 1.0,
 
   // This sets the sample rate to be 10%. You may want this to be 100% while
   // in development and sample at a lower rate in production
-  replaysSessionSampleRate: 1.0,
+  replaysSessionSampleRate: 0.1,
 
   // You can remove this option if you're not planning to use the Sentry Session Replay feature:
   integrations: [
